13903951b8f28e48c00f9b3599281f44596dd798
HernanRestrepo95 <hernanrestreporojas@gmail.com>
2021-06-21 11:07:42 -0500

    Hernan Restrepo
    Ajuste Hystrix

--- shopping-service/src/main/java/academy/digitallab/store/shopping/client/CustomerClient.java
+++ shopping-service/src/main/java/academy/digitallab/store/shopping/client/CustomerClient.java
@@ -1,15 +1,14 @@
 package academy.digitallab.store.shopping.client;
 
+import academy.digitallab.store.shopping.model.Customer;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import academy.digitallab.store.shopping.model.Customer;
-
-@FeignClient(name = "customer-service", fallback = CustomerHystrixFallBackFactory.class)
+@FeignClient(name = "customer-service", fallback = CustomerHystrixFallbackFactory.class)
 public interface CustomerClient {
 
-	@GetMapping(value = "/customers/{id}")
+    @GetMapping(value = "/customers/{id}")
     public ResponseEntity<Customer> getCustomer(@PathVariable("id") long id);
 }
